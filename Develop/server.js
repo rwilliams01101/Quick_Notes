da34f88b62c332c9c40864193d1deb562c2fd55c
RAW <rwilliams01101@gmail.com>
2020-04-02 08:30:50 -0500

    corrected CSS issue, save button issue

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,8 @@ var path = require("path");
 // import fs from the node library
 var fs = require("fs");
 // import db.json file into the server to reference
-var json = require("./db/db.json")
+var noteDb = require("./db/db.json")
+// console.log(noteDb);
 
 // instantiate a new express app utilizing the express() method
 var app = express();
@@ -26,16 +27,38 @@ app.use(express.json());
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "public/notes.html"));
     });
-    // return the notes.html file
+    // failsafe route
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "public/index.html"));
     });
 
+//  create the API get routes
+    // set up the notes API
+    app.get("/api/notes", function (req,res) {
+        // response needs to access the noteDB variable in order to send a response
+        res.json(noteDb);
+    })
+
 // create the POST routes
 
-// create the DELETE routes
+    app.post("/api/notes", function(req, res) {
+        noteDb.push(req.body);
+        // add the new note the db.json file
+        fs.readFile("/db/db.json", "utf-8", function (error){
+        });
+        fs.writeFile("/db/db.json", JSON.stringify(noteDb), function(error) {
+            req.body
+        });
+        res.json(noteDb);
+    });
 
-// start the server to listen
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+    // create the DELETE routes
+    
+    // app.delete("/api/notes", function(req, res) {
+    //     noteDb.pop();
+    // })
+    
+    // start the server to listen
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
