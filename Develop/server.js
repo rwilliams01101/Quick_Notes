712e7eab0f84fc70bc6d688068abe32f0250da6c
RAW <rwilliams01101@gmail.com>
2020-04-03 08:43:40 -0500

    notes rendering to page, json database updating properly

--- Develop/server.js
+++ Develop/server.js
@@ -5,15 +5,15 @@ var path = require("path");
 // import fs from the node library
 var fs = require("fs");
 // import db.json file into the server.js file to reference and use fs readFileSync to read contents of the file
-var data = fs.readFileSync("./db/db.json");
+var data = fs.readFileSync("./db/db.json", "utf8");
 // parse the json file
 var noteDb = JSON.parse(data);
-
+// since noteDb is an object, we need to turn it into an array of objects
+var finalNotesDB = Object.keys(noteDb).map(i => noteDb[i]);
 // instantiate a new express app utilizing the express() method
 var app = express();
 // declare PORT number so local server can find the application
 var PORT = 8080;
-
 // middleware parses the request string and converts to a JSON object
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,48 +22,43 @@ app.use(express.static("public"));
 // create the get routes
 
 // return the index.html file
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "public/index.html"));
-    });
-    // return the notes.html file
-    app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "public/notes.html"));
-    });
-    // failsafe route
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "public/index.html"));
-    });
-
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+// return the notes.html file
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 //  create the API get routes
-    // set up the notes API
-    app.get("/api/notes", function (req,res) {
-        // response needs to access the noteDB variable in order to send a response
-        res.json(noteDb);
-    })
+// set up the notes API
+app.get("/api/notes", function(req, res) {
+  // response needs to access the noteDB variable in order to send a response
+  res.json(finalNotesDB);
+});
+// failsafe route
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 // create the POST routes
 
-    app.post("/api/notes", function(req, res) {
-       
-        var newNote = req.body;
-        // console.log('New Note: ', req.body)
-        
-        noteDb.push(newNote);
-        // need to read db.json file
-        // fs.readFile(__dirname, "db/db.json")
-        // append newNote to db.json
-        // fs.readFileSync("db/db.json", JSON.stringify(newNote))
-        fs.appendFileSync("db/db.json", ',' + '\n' + JSON.stringify([newNote]))
-        res.json(newNote);
-    }); 
+app.post("/api/notes", function(req, res) {
+  noteDb.push(req.body);
 
-    // create the DELETE routes
-    
-    // app.delete("/api/notes", function(req, res) {
-    //     noteDb.pop();
-    // })
-    
-    // start the server to listen
-    app.listen(PORT, function() {
-        console.log("App listening on PORT " + PORT);
-    });
+  fs.writeFile("./db/db.json", JSON.stringify(noteDb), function(error) {
+    if (error) {
+      console.log("Your note did not save.");
+    }
+  });
+  res.json(finalNotesDB);
+});
+
+// create the DELETE routes
+// app.delete("/api/notes/delete", function(req, res) {
+//   finalNotesDB.id = res.send("Your note has been deleted");
+// });
+
+// start the server to listen
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
